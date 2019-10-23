821516e1c7f46bc45955cc830ae1c527e30e5ae1
Thyrael <robin.baraffe@gmail.com>
2019-10-23 13:01:59 +0200

    Add TU writeOutput file

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
   "testing"
   "os"
   "bufio"
+  "strings"
 )
 
 var path string = "list.txt"
@@ -16,9 +17,48 @@ func TestOpenFile(t *testing.T) {
 
 
 func TestReadFile(t *testing.T) {
-  if file, err := OpenFile(path)
+  file := OpenFile(path)
   defer file.Close()
 
   scanner := bufio.NewScanner(file)
   AnalyzeFile(scanner)
+}
+
+func TestAnalyzeFile(t *testing.T) {
+
+  lines := []string{}
+  duplicateLines := []string{}
+
+  file := OpenFile(path)
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+      line := scanner.Text()
+      findDuplicateLine := false
+
+      if len(lines) != 0 {
+          for i := range lines {
+              if strings.Compare(lines[i], line) == 0 {
+                  duplicateLines = append(duplicateLines, line)
+                  findDuplicateLine = true
+                  break
+              }
+          }
+      }
+
+      if !findDuplicateLine {
+          lines = append(lines, line)
+      }
+  }
+}
+
+func TestWriteOutput(t *testing.T) {
+  file, err := os.Create("TestWriteOutput.txt")
+  if err != nil {
+    t.Error("Something wrong with file creation")
+  }
+  dataWriter := bufio.NewWriter(file)
+  _, _ = dataWriter.WriteString("test" + "\r\n")
+
 }
